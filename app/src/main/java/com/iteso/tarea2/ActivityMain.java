037496cd2290a52dec071031d2d4bfd65be884b2
Jorge Perez <alberto.perezf2@gmail.com>
2018-09-14 01:03:43 -0500

    Added image, title and like, color radio buttons and like toast.

--- app/src/main/java/com/iteso/tarea2/ActivityMain.java
+++ app/src/main/java/com/iteso/tarea2/ActivityMain.java
@@ -2,6 +2,8 @@ package com.iteso.tarea2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class ActivityMain extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class ActivityMain extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void Color(View v){
+
+    }
+
+    public void Like(View v){
+        Toast.makeText(this, R.string.like, Toast.LENGTH_LONG).show();
     }
 }
